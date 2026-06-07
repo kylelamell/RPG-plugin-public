fdf43832ab09de8f1065284d2d79a148b639d926
Kyle Lamell <qphys2024@gmail.com>
2026-06-07 16:18:34 -0400

    refactor parser to use common abstractions

--- src/main/kotlin/com/rpgle/plugin/parser/RpgParserDefinition.kt
+++ src/main/kotlin/com/rpgle/plugin/parser/RpgParserDefinition.kt
@@ -1,40 +1,31 @@
 package com.rpgle.plugin.parser
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement
-import com.intellij.lang.ASTNode
-import com.intellij.lang.ParserDefinition
-import com.intellij.lang.PsiParser
 import com.intellij.lexer.Lexer
 import com.intellij.openapi.project.Project
 import com.intellij.psi.FileViewProvider
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.tree.IFileElementType
 import com.intellij.psi.tree.TokenSet
 import com.rpgle.plugin.RpgLanguage
+import com.rpgle.plugin.common.AbstractFlatParserDefinition
 import com.rpgle.plugin.lexer.RpgLexerAdapter
 import com.rpgle.plugin.psi.RpgFile
-import com.rpgle.plugin.psi.RpgTokenSets
+import com.rpgle.plugin.psi.RpgTokenTypes
 
 /**
- * Lightweight parser definition: the lexer does the real work and the parser produces a flat PSI
- * (all tokens as leaf children of the file root), enough for highlighting, completion and the rest.
+ * Lightweight parser definition. The lexer does the real work; the shared
+ * [AbstractFlatParserDefinition] produces a flat PSI (all tokens as leaf children
+ * of the file root), which is enough for highlighting, the annotator, completion,
+ * commenting and brace matching without a full grammar.
  */
-class RpgParserDefinition : ParserDefinition {
+class RpgParserDefinition : AbstractFlatParserDefinition(
+    TokenSet.create(RpgTokenTypes.COMMENT),
+    TokenSet.create(RpgTokenTypes.STRING),
+) {
 
     override fun createLexer(project: Project?): Lexer = RpgLexerAdapter()
 
-    override fun createParser(project: Project?): PsiParser = RpgFlatParser()
-
     override fun getFileNodeType(): IFileElementType = FILE
-
-    override fun getCommentTokens(): TokenSet = RpgTokenSets.COMMENTS
-
-    override fun getStringLiteralElements(): TokenSet = RpgTokenSets.STRINGS
-
-    override fun getWhitespaceTokens(): TokenSet = RpgTokenSets.WHITESPACES
-
-    override fun createElement(node: ASTNode): PsiElement = ASTWrapperPsiElement(node)
 
     override fun createFile(viewProvider: FileViewProvider): PsiFile = RpgFile(viewProvider)
 
